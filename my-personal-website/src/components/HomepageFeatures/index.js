1c66f573e4d9e367c6a824c851ce5b7a72608a99
nunohpinheiro <nunohpinheiro@outlook.com>
2023-10-22 18:34:55 +0100

    Fix Homepage elements classNames

--- my-personal-website/src/components/HomepageFeatures/index.js
+++ my-personal-website/src/components/HomepageFeatures/index.js
@@ -8,23 +8,23 @@ export default function HomepageFeatures() {
     <section className={styles.features}>
       <div className="container">
         <div className="row .custom-big-clear">
-          <div class="div-centered">
+          <div className="div-centered">
             <div className={clsx('col--4')}>
               <div className={styles.buttons}>
                 <Link
                   className="button button--secondary button--lg"
                   to="https://linkedin.com/in/nunohpinheiro">
                   Get to know me&nbsp;
-                  	<img src="img/linkedin-48.png" class="img-top-static" height="30" width="30"/>
-                    <img src="img/linkedin-noback.gif" class="img-top" height="30" width="30"/>
+                  	<img src="img/linkedin-48.png" className="img-top-static" height="30" width="30"/>
+                    <img src="img/linkedin-noback.gif" className="img-top" height="30" width="30"/>
                 </Link>
                 &nbsp;&nbsp;&nbsp;
                 <Link
                   className="button button--secondary button--lg"
                   to="/hi-there/my-personal-links">
                   Check the links&nbsp;
-                    <img src="img/links-48.png" class="img-top-static" height="30" width="30"/>
-                    <img src="img/links-noback.gif" class="img-top" height="30" width="30"/>
+                    <img src="img/links-48.png" className="img-top-static" height="30" width="30"/>
+                    <img src="img/links-noback.gif" className="img-top" height="30" width="30"/>
                 </Link>
               </div>
             </div>
