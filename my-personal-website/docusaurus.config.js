5a014b87b118e02dfdba85e29740c868a34d1cbb
Nuno Pinheiro <npinheiro@letsgetchecked.com>
2023-10-23 11:36:25 +0100

    Set docs sidebar as hideable, Remove unnecessary navbar content labels

--- my-personal-website/docusaurus.config.js
+++ my-personal-website/docusaurus.config.js
@@ -58,6 +58,11 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: true,
